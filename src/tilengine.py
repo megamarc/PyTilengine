bf90916a23d12bc76f8435c223067ab15de65b72
megamarc <megamarc@hotmail.com>
2018-10-10 09:49:07 +0200

    Updates MIT license info inside the binding header

--- src/tilengine.py
+++ src/tilengine.py
@@ -4,6 +4,28 @@ Updated to library version 1.21.0
 http://www.tilengine.org
 """
 
+"""
+Copyright (c) 2018 Marc Palacios Domènech
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 # pylint: disable=C0103
 # pylint: disable=W0614
 # pylint: disable=W0312
@@ -11,24 +33,6 @@ http://www.tilengine.org
 from sys import platform as _platform
 from ctypes import *
 
-"""
-* Tilengine - The 2D retro graphics engine with raster effects
-* Copyright (C) 2015-2018 Marc Palacios Domenech <mailto:megamarc@hotmail.com>
-* All rights reserved
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Library General Public License for more details.
-*
-* You should have received a copy of the GNU Library General Public
-* License along with this library. If not, see <http://www.gnu.org/licenses/>.
-"""
 
 # constants --------------------------------------------------------------------
 
